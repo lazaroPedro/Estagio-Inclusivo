01a6b695f9e534a54f36c9ef403f57b9dec1cee4
Clara-Maciel <mc.macie@outlook.com>
2025-05-04 21:08:13 -0300

    CadastroVagasServlet

--- src/main/java/com/ifbaiano/estagioinclusivo/controller/servlet/CadastroVagaServlet.java
+++ src/main/java/com/ifbaiano/estagioinclusivo/controller/servlet/CadastroVagaServlet.java
@@ -1,64 +1,84 @@
 package com.ifbaiano.estagioinclusivo.controller.servlet;
 
 import java.io.IOException;
+import java.sql.Connection;
 
+import com.ifbaiano.estagioinclusivo.config.DBConfig;
 import com.ifbaiano.estagioinclusivo.dao.DAOVaga;
 import com.ifbaiano.estagioinclusivo.model.Vaga;
 import com.ifbaiano.estagioinclusivo.model.dto.SessionDTO;
 import com.ifbaiano.estagioinclusivo.model.enums.TipoVaga;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
 
 @WebServlet("/CadastroVagaServlet")
+
 public class CadastroVagaServlet extends HttpServlet {
 
-	private Object enderecoId;
+	private Connection connection;
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UFT-8");
-		
-		try {
-			HttpServlet session = (HttpServlet) request.getSession(false);
-			SessionDTO usuariologado = (session != null) ? (SessionDTO) ((ServletRequest) session).getAttribute("usuarioLogado") : null;
-		
-			if (usuarioLogado == null || usuarioLogado.getTipoUsuario() == null) {
-				request.setRedirect("login.jsp");
+
+			HttpSession session = request.getSession(false);
+			SessionDTO usuariologado = (session != null) ? (SessionDTO)session.getAttribute("usuarioLogado") : null;
+			
+			if (usuariologado == null || usuariologado.getTipoUsuario() == null) {
+				response.sendRedirect("login.jsp");
 				return;
-			} 
-
-		String descricao = request.getParameter("descricao");
-		String requisitos = request.getParameter("requisitos");
-		String beneficios = request.getParameter("beneficios");
-		int qtdVagas = Integer.parseInt(request.getParameter("qtd_vagas"));
-		TipoVaga status = request.getParameter("status");
-		int Empresa.id - Integer.parseInt(request.getParameter("empresa_id"));
-		int idEndereco - Integer.parseInt(request.getParameter("endereco_id"));
-
-		Vaga vaga = new Vaga();
-		vaga.setDescricao(descricao);
-		vaga.setRequisitos(requisitos);
-		vaga.setBeneficios(beneficios);
-		vaga.setQtdVagas(qtdVagas);
-		vaga.setStatus(status);
-		vaga.setId(idVaga);
+			}
 		
-		DAOVaga vagaDAO = new DAOVaga();
-		vagaDAO.inserir(vaga);
 		
-		request.setAttribute("mensagem", "Vaga cadastrada com sucesso!");
-		request.getRequestDispatcher("CadastroVagas.jsp").forward(request, response);
-		
-	}catch(Exception e){
-		e.printStackTrace();
-		request.setAttribute("erro", "Erro ao cadastraar vaga: " + e.getMessage());
-		request.getRequestDispatcher("CadastroVagas.jsp").forward(request, response);
 
-	}
+		try (Connection connection = DBConfig.criarConexao()){
+			connection.setAutoCommit(false);
+			
+			String descricao = request.getParameter("descricao");
+			String requisitos = request.getParameter("requisitos");
+			String beneficios = request.getParameter("beneficios");
+			int qtdVagas = Integer.parseInt(request.getParameter("qtd_vagas"));
+			String statusParam = request.getParameter("status");
+			String empresaNome = request.getParameter("empresa_nome");
+
+			
+			if (descricao == null || descricao.trim().isEmpty() ||
+					requisitos == null || requisitos.trim().isEmpty() ||
+					beneficios == null || beneficios.trim().isEmpty() || 
+					statusParam == null || statusParam.trim().isEmpty() || 
+					empresaNome == null || empresaNome.trim().isEmpty()) {
+				
+				
+				request.setAttribute("erro", "todos os campos são obrigatórios.");
+				request.getRequestDispatcher("CadastroVagas.jsp").forward(request, response);
+				return;
+			}
+
+			TipoVaga status = TipoVaga.valueOf(statusParam.toUpperCase());
+
+			Vaga vaga = new Vaga();
+			vaga.setDescricao(descricao);
+			vaga.setRequisitos(requisitos);
+			vaga.setBeneficios(beneficios);
+			vaga.setQtdVagas(Integer.parseInt(request.getParameter("qtd_vagas")));
+			vaga.setStatus(status);
+		
+
+			
+			
+			DAOVaga vagaDAO = new DAOVaga(connection);
+			vagaDAO.insert(vaga);
+
+			request.setAttribute("mensagem", "Vaga cadastrada com sucesso!");
+			request.getRequestDispatcher("CadastroVagas.jsp").forward(request, response);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			request.setAttribute("erro", "Erro ao cadastraar vaga: " + e.getMessage());
+			request.getRequestDispatcher("CadastroVagas.jsp").forward(request, response);
+
+		}
+		
 }
-
+}
